0776329f4e4e15b1c9c080590c308f4a9ecd71b1
AldoSanchez36 <62495084+AldoSanchez36@users.noreply.github.com>
2021-11-13 08:23:55 -0600

    Add files via upload
    index.js crear carpeta src
    cc,htm creat carpeta public

---
+++ index.js
@@ -0,0 +1,12 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(
+  <div>
+    <h1>hello </h1>
+    <ul>1</ul>
+    <ul>2</ul>
+    <ul>3</ul>
+  </div>,
+  document.getElementById("root")
+);
